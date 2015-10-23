e19baf83728c4fa6328bdd97085a1e4cba55db58
joshbyrd <jbyrd004@seattlecnetral.edu>
2015-10-22 23:49:35 -0700

    adding php to template

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php 
+
+?>
+<footer>
+	<ul>
+    <li><a href="index.html">Copyright</a></li>
+    <li><a href="">All Rights Reserved</a></li>
+    <li><a href="">Disclaimer</a></li>
+	<li><a href="">Web Design by SSYC WebTeam</a></li>
+    <li><a href="">CONTACT</a></li>
+	</ul>
+</footer>
+
+<?php wp_footer();?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<?php
+/*
+    Template Designed by SSYC WebTeam
+    Created for ITC210/WEB120
+    Credits:
+    version 1.0
+*/
+?><!DOCTYPE html>
+
+<html>
+
+<head>
+<link href='http://fonts.googleapis.com/css?family=Varela+Round|Carter+One|Kalam|Lemon|Monoton|Wire+One|Tulpen+One' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Audiowide|Michroma|Julius+Sans+One' rel='stylesheet' type='text/css'>
+<title>Seattle Singles YC</title>
+
+<link rel="stylesheet"  href="style.css" >
+</head>
+
+<body>
+
+<header>
+
+<div id="logo">
+<p>SSYC</p>
+</div>
+    
+    <nav>
+
+	<ul>
+    <li><a href="index.html">HOME</a></li> 
+    <li><a href="">ABOUT US</a></li>
+    <li><a href="">MEMBERSHIP</a></li>
+	<li><a href="">BOATING</a></li>
+    <li><a href="">EVENTS</a></li>
+    <li><a href="">CONTACT US</a></li>
+	</ul>
+
+	</nav>
+
+</header>
